885980e6afef0f5e3c4cf68ec7c73196c6221fb1
kaan <kaanantt@gmail.com>
2016-11-13 20:06:23 +0200

    +++

---
+++ app/Http/Controllers/Site/OrderController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Site;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+
+class OrderController extends Controller
+{
+
+    function cartOrder(){
+
+        if (!Auth::check()) {
+            return redirect(action("Site\\AuthController@showlogin"));
+        }
+
+        return 0;
+    }
+
+}
