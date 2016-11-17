d284ddf6c2d353d71d711eb6ec26023d9fcc25c1
hcancakici <hcancakici@gmail.com>
2016-11-17 19:15:22 +0200

    Admin middleware

--- app/Http/Middleware/CheckAdmin.php
+++ app/Http/Middleware/CheckAdmin.php
@@ -15,14 +15,9 @@ class CheckAdmin
      */
     public function handle($request, Closure $next)
     {
-<<<<<<< HEAD
-        if(!$request->user()->isAdmin()){
+        if(!$request->user()->isAdmin()) {
             return redirect('admin');
-=======
-        /*if(!$request->user()->isAdmin()){
-            return redirect('admin.login');
->>>>>>> ea71ba372f884a4820ccf57ea6fe530fcbc08fba
         }
-        return $next($request);*/
+        return $next($request);
     }
 }
